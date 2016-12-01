9b1f1704cc92d9231128ebe9dc8f5a441102df5f
muto <muto@sushi.lab.kinulab.org>
2016-12-01 15:58:18 +0900

    ローマ数字をアルファベットに変換する処理追加

--- kyuko.py
+++ kyuko.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 class KyukoInfo:
+    
     def __init__(self):
         self.date = None
         self.department = None
@@ -41,16 +42,22 @@ class KyukoInfo:
 
 def fetchKyukoInfo():
     url = 'http://hirose.sendai-nct.ac.jp/kyuko/kyuko.cgi'
-    html = requests.get(url).content
-    soup = BeautifulSoup(html, 'html5lib')
+    html = requests.get(url)
+    html.encoding = 'shif_jis'
+    soup = BeautifulSoup(html.content, 'html5lib')
     attrs = {'width': 650}
     kyuko = list()
+    num_map = {'�T': 'I', '�U': 'II', '�V': 'III'}
     for elem in soup.findAll('table', attrs):
         detail = KyukoInfo()
         detail.date = list(re.findall('([0-9]{,2})月([0-9]{,2})日<.*>\((.*)\)</font>', str(elem.b))[0])
         detail.department, detail.time = [x.string for x in elem.select('td font b')]
         detail.teacher = elem.select('td font[color=#00008B]')[0].string.strip()
-        detail.subject = elem.select('tr td tr font')[0].string
+        subject = elem.select('tr td tr font')[0].string
+        for num in num_map:
+            if num in subject:
+                subject = subject.replace(num, num_map[num])
+        detail.subject = subject
         detail.status = re.findall('./img/(.*).gif', str(elem.select('img')))[0]
         kyuko.append(detail)
     return kyuko
